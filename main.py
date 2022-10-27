6f0ecda97e413681299761f51aa1f111588510f4
Igor Silva <igor.victor31@hotmail.com>
2022-10-27 01:54:34 -0300

    second commit

--- main.py
+++ main.py
@@ -1,127 +1,59 @@
 from urllib import response
-
 from typing import List
-
 from fastapi import FastAPI
-
 import databases
-
 import sqlalchemy
-
 from pydantic import BaseModel
-
 
 DATABASE_URL = "postgresql://uymanekmiqgufv:6d865d40b8b51f1ec8496cc19c8501860b573ac56e6bfd8c12c4867e6ce0a019@ec2-52-207-90-231.compute-1.amazonaws.com:5432/d2b9npisdb21te"
 
 database = databases.Database(DATABASE_URL)
 
-
 metadata = sqlalchemy.MetaData()
 
-
- 
-
 notes = sqlalchemy.Table(
-
     "notes",
-
     metadata,
-
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-
     sqlalchemy.Column("text", sqlalchemy.String),
-
     sqlalchemy.Column("completed", sqlalchemy.Boolean)
-
 )
-
-
- 
 
 engine = sqlalchemy.create_engine(
-
     DATABASE_URL
-
 )
-
-
- 
 
 metadata.create_all(engine)
 
-
- 
-
 class Note(BaseModel):
-
     id: int
-
     text: str
-
     completed: bool
-
-    
 
 class NoteIn(BaseModel):
-
     text: str
-
     completed: bool
-
-    
+ 
 
 app = FastAPI()
 
-
- 
-
 @app.on_event("startup")
-
 async def startup():
-
     await database.connect()
 
-    
-
-    
 
 @app.on_event("shutdown")
-
 async def shutdown():
-
     await database.disconnect()
-
-    
+  
 
 @app.get("/notes/", response_model=List[Note])   
-
 async def read_notes():
-
     query = notes.select()
-
     return await database.fetch_all(query) 
 
-
- 
-
-@app.get("/notes/", response_model=List[Note])   
-
+@app.post("/notes/", response_model=List[Note])   
 async def create_notes(note: NoteIn):
-
     query = notes.insert().values(text=note.text, completed=note.completed)
-
     last_record_id = await database.execute(query)
-
     return {**note.dict(), "id": last_record_id}
-
-
- 
-
-@app.get("/")
-
-
- 
-
-async def index():
-
-    return {"Message": "Easter Science"}
