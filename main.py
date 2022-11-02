d2b90a8d1771aaf2ace451b59e26bb6da211f27f
Igor Silva <igor.victor31@hotmail.com>
2022-11-02 00:02:12 -0300

    teste1

--- main.py
+++ main.py
@@ -5,19 +5,11 @@ import databases
 import sqlalchemy
 from pydantic import BaseModel
  
-DATABASE_URL = "postgresql://qlulfgqisaplnd:38f7bf02196cb17600a3fa97329848a2c628c73856182ce4f8e40582387042b1@ec2-44-195-132-31.compute-1.amazonaws.com:5432/d1eivhichip29"
+DATABASE_URL = "postgresql://qvqtkcccqbhixf:07767eb05b3f44bbfea04d029da3a3ac390e7dc9122331d497c8ad1482e1c7f6@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d6qrgl98nj9om6"
 
 database = databases.Database(DATABASE_URL)
 
 metadata = sqlalchemy.MetaData()
-
-notes = sqlalchemy.Table(
-    "notes",
-    metadata,
-    sqlalchemy.Column("name", sqlalchemy.String, primary_key=True),
-    sqlalchemy.Column("img", sqlalchemy.String),
-    sqlalchemy.Column("level", sqlalchemy.String)
-)
 
 engine = sqlalchemy.create_engine(
     DATABASE_URL
@@ -25,32 +17,28 @@ engine = sqlalchemy.create_engine(
 
 metadata.create_all(engine)
 
-class Note(BaseModel):
-    name: str
-    img: str
-    level: str
-
-
 app = FastAPI()
 
+menu = sqlalchemy.Table(
+    "menu",
+    metadata,
+    sqlalchemy.Column("nome", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("img", sqlalchemy.String),
+)
+
+class Menu(BaseModel):
+    nome: str
+    img: str
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
 
-
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
   
-
-@app.get("/notes/", response_model=List[Note])   
-async def read_notes():
-    query = notes.select()
+@app.get("/menu/", response_model=List[Menu])   
+async def read_restaurantes():
+    query = menu.select()
     return await database.fetch_all(query) 
-
-@app.post("/notes/", response_model=List[Note])   
-async def create_notes(note: Note):
-    query = notes.insert().values(name=note.name, img=note.img, level=note.level)
-    last_record_name = await database.execute(query)
-    return {**note.dict(), "name": last_record_name}
